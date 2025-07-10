cc11359c99f21c756463c26c3f1f1d8aaedaa9c9
tgodefro <tgodefro@student.42lyon.fr>
2025-07-10 18:08:07 +0200

    c01/Ex01

---
+++ c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgodefro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/10 16:53:47 by tgodefro          #+#    #+#             */
+/*   Updated: 2025/07/10 16:53:49 by tgodefro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*int	main(void)
+{
+	int	test = 69;
+	int	*test_p1 = &test;
+	int	**test_p2 = &test_p1;
+	int	***test_p3 = &test_p2;
+	int	****test_p4 = &test_p3;
+	int	*****test_p5 = &test_p4;
+	int	******test_p6 = &test_p5;
+	int	*******test_p7 = &test_p6;
+	int	********test_p8 = &test_p7;
+	int	*********test_p9 = &test_p8;
+	printf("test: %d\n", test);
+	printf("test_p: %p\n", test_p9);
+	printf("*test_p: %d\n", *********test_p9);
+	printf("ft_ultimate_ft\n");
+	ft_ultimate_ft(test_p9);
+	printf("test: %d\n", test);
+	printf("test_p: %p\n", test_p9);
+	printf("*test_p: %d\n", *********test_p9);
+}*/
