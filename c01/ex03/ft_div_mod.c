0e5a1cc98f1a13d66f492c3d7c92c52edcce25b9
tgodefro <tgodefro@student.42lyon.fr>
2025-07-10 18:41:24 +0200

    c01/Ex03

--- c01/ex03/ft_div_mod.c
+++ c01/ex03/ft_div_mod.c
@@ -1,45 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/10 18:10:00 by tgodefro          #+#    #+#             */
-/*   Updated: 2025/07/10 18:40:02 by tgodefro         ###   ########.fr       */
+/*   Created: 2025/07/10 18:30:38 by tgodefro          #+#    #+#             */
+/*   Updated: 2025/07/10 18:40:39 by tgodefro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stdio.h>
 
-void	ft_swap(int *a, int *b)
+void	ft_div_mod(int a, int b, int *div, int *mod)
 {
-	int	temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
+	*div = a / b;
+	*mod = a % b;
 }
 
 /*int	main(void)
 {
-	int	a;
-	int	b;
-	int	*a_p;
-	int	*b_p;
+	int	div;
+	int	mod;
+	int	*d_p;
+	int	*m_p;
 
-	a = 42;
-	a_p = &a;
-	b = 69;
-	b_p = &b;
-	printf("a: %d\n", a);
-	printf("a_p: %p\n", a_p);
-	printf("b: %d\n", b);
-	printf("b_p: %p\n", b_p);
-	printf("--- ft_swap\n");
-	ft_swap(a_p, b_p);
-	printf("a: %d\n", a);
-	printf("a_p: %p\n", a_p);
-	printf("b: %d\n", b);
-	printf("b_p: %p\n", b_p);
+	d_p = &div;
+	m_p = &mod;
+	ft_div_mod(18, 7, d_p, m_p);
+	printf("div: %d\n", div);
+	printf("mod: %d\n", mod);
 }*/
