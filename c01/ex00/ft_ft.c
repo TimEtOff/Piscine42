c021f622b2b4a09f982d50480608b3d4bb4b2b80
tgodefro <tgodefro@student.42lyon.fr>
2025-07-10 16:51:26 +0200

    c01/Ex00

---
+++ c01/ex00/ft_ft.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgodefro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/10 16:36:21 by tgodefro          #+#    #+#             */
+/*   Updated: 2025/07/10 16:36:22 by tgodefro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+/*int	main(void)
+{
+	int	test = 69;
+	int	*test_p = &test;
+	printf("test: %d\n", test);
+	printf("test_p: %p\n", test_p);
+	printf("*test_p: %d\n", *test_p);
+	printf("ft_ft\n");
+	ft_ft(test_p);
+	printf("test: %d\n", test);
+	printf("test_p: %p\n", test_p);
+	printf("*test_p: %d\n", *test_p);
+}*/
