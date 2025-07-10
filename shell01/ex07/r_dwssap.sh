140586d0fe0dfd77bf01194895dd2b5294b69322
tgodefro <tgodefro@student.42lyon.fr>
2025-07-10 10:59:10 +0200

    shell01/Fixed ex07 after evaluation (i guess)

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/passwd | sed "#*" | sed -n 'p;n' | cut -d ":" -f 1 | rev | sort -r | sed -n {${FT_LINE1},${FT_LINE2}p} | tr "\n" "," | sed 's/,/, /g' | sed 's/, $/\./' | tr -d "\n"
+cat /etc/passwd | sed "#*" | sed -n 'p;n' | cut -d ":" -f 1 | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | tr "\n" "," | sed 's/,/, /g' | sed 's/, $/\./' | tr -d "\n"
