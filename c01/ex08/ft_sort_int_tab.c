7d80b5a8b20dd44080e8cc6cda8ebf24d4ce1ff7
tgodefro <tgodefro@student.42lyon.fr>
2025-07-11 11:55:10 +0200

    c01/Ex08

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -1,28 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/11 09:35:47 by tgodefro          #+#    #+#             */
-/*   Updated: 2025/07/11 10:15:58 by tgodefro         ###   ########.fr       */
+/*   Created: 2025/07/11 10:15:28 by tgodefro          #+#    #+#             */
+/*   Updated: 2025/07/11 11:54:40 by tgodefro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stdio.h>
 
-void	ft_rev_int_tab(int *tab, int size)
+int	ft_min_i_int_tab(int *tab, int start, int size)
+{
+	int	i;
+	int	min_i;
+	int	min_val;
+
+	i = start + 1;
+	min_i = start;
+	min_val = tab[min_i];
+	while (i < size)
+	{
+		if (tab[i] < min_val)
+		{
+			min_i = i;
+			min_val = tab[min_i];
+		}
+		i++;
+	}
+	return (min_i);
+}
+
+void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
 	int	temp;
+	int	min;
 
 	i = 0;
-	while (i < (size / 2))
+	while (i < size)
 	{
-		temp = tab[i];
-		tab[i] = tab[size - i - 1];
-		tab[size - i - 1] = temp;
+		min = ft_min_i_int_tab(tab, i, size);
+		temp = tab[min];
+		tab[min] = tab[i];
+		tab[i] = temp;
 		i++;
 	}
 }
@@ -43,13 +66,13 @@ void	ft_rev_int_tab(int *tab, int size)
 
 int	main(void)
 {
-	int	tab[6] = {0, 1, 2, 3, 4, 5};
-	int	tab2[7] = {0, 1, 2, 3, 4, 5, 6};
+	int	tab[6] = {4, 6, 8, 7, 1};
+	int	tab2[7] = {9, 4, 2, 3, 6, 8};
 
 	print_tab(tab, 6);
-	ft_rev_int_tab(tab, 6);
+	ft_sort_int_tab(tab, 6);
 	print_tab(tab, 6);
 	print_tab(tab2, 7);
-	ft_rev_int_tab(tab2, 7);
+	ft_sort_int_tab(tab2, 7);
 	print_tab(tab2, 7);
 }*/
