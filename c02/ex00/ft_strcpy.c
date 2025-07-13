5081fbe09887ab91094a9c3f3fc587982b9cb8ff
tgodefro <tgodefro@student.42lyon.fr>
2025-07-13 15:06:41 +0200

    c02/ex00

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -10,4 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 
+/*
+From `man strcpy`:
+> The strcpy() function copies the string pointed to by src,
+> including the terminating null byte ('\0'), to the buffer pointed to by dest.
+> The strings may not overlap, and the destination string dest must be large
+> enough to receive the copy.
+*/
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
+
+int	main(void)
+{
+	char	src[] = "This is a test";
+	char	dest[] = "";
+
+	printf("src:	%s\n", src);
+	printf("dest:	%s\n", dest);
+	printf("ft_strcpy: %s\n", ft_strcpy(dest, src));
+	printf("src:	%s\n", src);
+	printf("dest:	%s\n", dest);
+}
