814fdd2cb1c38db9a3e7059f433021973777fc32
tgodefro <tgodefro@student.42lyon.fr>
2025-07-13 16:48:33 +0200

    rush00/Bonus subjects

---
+++ rush00/ex00/rush04.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/13 16:46:11 by tgodefro          #+#    #+#             */
+/*   Updated: 2025/07/13 16:48:14 by tgodefro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_line_top(int x_max, int y_max, int y_current)
+{
+	int	increment;
+	int	upper_left;
+	int	upper_right;
+	int	bottom_left;
+	int	bottom_right;
+
+	increment = 0;
+	while (increment < x_max)
+	{
+		upper_left = ((increment == 0) && (y_current == 0));
+		upper_right = ((increment == (x_max - 1)) && (y_current == 0));
+		bottom_left = ((increment == 0) && (y_current == (y_max - 1)));
+		bottom_right = (increment == (x_max - 1));
+		bottom_right = bottom_right && (y_current == (y_max - 1));
+		if (upper_left)
+			ft_putchar('A');
+		else if (upper_right || bottom_left)
+			ft_putchar('C');
+		else if (bottom_right)
+			ft_putchar('A');
+		else
+			ft_putchar('B');
+		increment++;
+	}
+}
+
+void	ft_line(int x)
+{
+	int	increment;
+
+	increment = 0;
+	while (increment < x)
+	{
+		if ((increment == 0) || (increment == (x - 1)))
+			ft_putchar('B');
+		else
+			ft_putchar(' ');
+		increment++;
+	}
+}
+
+void	rush(int x, int y)
+{
+	int	incr_y;
+
+	incr_y = 0;
+	while (incr_y < y)
+	{
+		if (incr_y == 0 || incr_y == (y - 1))
+			ft_line_top(x, y, incr_y);
+		else
+			ft_line(x);
+		ft_putchar('\n');
+		incr_y++;
+	}
+}
+
