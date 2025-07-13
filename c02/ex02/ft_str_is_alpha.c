5ece78a42559b7398b7a76f3a1c4e371918cb156
tgodefro <tgodefro@student.42lyon.fr>
2025-07-13 15:30:01 +0200

    c02/Ex02

---
+++ c02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/13 15:14:00 by tgodefro          #+#    #+#             */
+/*   Updated: 2025/07/13 15:29:38 by tgodefro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	res;
+
+	res = 1;
+	while (*str != '\0')
+	{
+		if (!((((int) 'a' <= (int) *str) && ((int) *str <= (int) 'z'))
+				|| (((int) 'A' <= (int) *str) && ((int) *str <= (int) 'Z'))
+				|| ((int) ' ' == (int) *str)
+				|| (((int) '0' <= (int) *str) && ((int) *str <= (int) '9'))
+			)
+		)
+		{
+			res = 0;
+		}
+		str++;
+	}
+	return (res);
+}
+
+/*int	main(void)
+{
+	printf("%i\n", ft_str_is_alpha("Tdfhfzhzd"));
+	printf("%i\n", ft_str_is_alpha("This is only alphanumeric chars"));
+	printf("%i\n", ft_str_is_alpha("This is only 161650 chars"));
+	printf("%i\n", ft_str_is_alpha("This is not only #$$@$& chars"));
+}*/
