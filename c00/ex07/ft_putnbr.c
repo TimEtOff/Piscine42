045cf691d9a87fff192b3aad27b5a7f707a50147
tgodefro <tgodefro@student.42lyon.fr>
2025-07-14 16:18:20 +0200

    c00/Ex07

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 11:58:44 by tgodefro          #+#    #+#             */
-/*   Updated: 2025/07/14 15:41:52 by tgodefro         ###   ########lyon.fr   */
+/*   Updated: 2025/07/14 16:18:00 by tgodefro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 void	ft_putnbr(int nb)
 {
+	char	c;
 
+	c = (char)(nb % 10 + 48);
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	ft_putnbr(751);
+	write(1, "\n", 1);
+	ft_putnbr(8);
+	write(1, "\n", 1);
+	ft_putnbr(18452);
+	write(1, "\n", 1);
+	return (0);
 }
