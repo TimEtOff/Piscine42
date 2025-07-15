2c401bf49cfc9e68e45220f6355346f50941a15a
tgodefro <tgodefro@student.42lyon.fr>
2025-07-15 10:57:24 +0200

    c02/Fixed ex00 and 01

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -6,11 +6,12 @@
 /*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 09:24:34 by tgodefro          #+#    #+#             */
-/*   Updated: 2025/07/14 17:43:31 by tgodefro         ###   ########lyon.fr   */
+/*   Updated: 2025/07/15 10:40:56 by tgodefro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
 
 /*
 From `man strcpy`:
@@ -33,12 +34,22 @@ char	*ft_strcpy(char *dest, char *src)
 	return (dest);
 }
 
-/*int	main(void)
+int	main(void)
 {
 	char	src[] = "This is a test";
 	char	dest[] = "";
+	char	src2[] = "This is a test";
+	char	dest2[] = "";
 
-	printf("src:	%s\n", src);
-	printf("ft_strcpy: %s\n", ft_strcpy(dest, src));
-	printf("dest:	%s\n", dest);
-}*/
+	printf("src:		%s\n", src);
+	printf("dest:		%s\n", dest);
+	printf("ft_strcpy:	%s\n", ft_strcpy(dest, src));
+	printf("src:		%s\n", src);
+	printf("dest:		%s\n", dest);
+	printf("\n");
+	printf("src2:		%s\n", src2);
+	printf("dest2:		%s\n", dest2);
+	printf("strcpy:		%s\n", strcpy(dest2, src2));
+	printf("src2:		%s\n", src2);
+	printf("dest2:		%s\n", dest2);
+}
--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,32 +6,60 @@
 /*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 15:07:55 by tgodefro          #+#    #+#             */
-/*   Updated: 2025/07/14 17:43:41 by tgodefro         ###   ########lyon.fr   */
+/*   Updated: 2025/07/15 10:57:06 by tgodefro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+/*#include <stdio.h>
+#include <string.h>*/
+
+/*
+	Edited version from `c01/ex06/ft_strlen.c`
+*/
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	nbr;
+
+	nbr = 0;
+	while (*str != '\0')
+	{
+		nbr++;
+		str++;
+	}
+	return (nbr);
+}
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
 
+	if (ft_strlen(dest) < n)
+		return (dest);
 	i = 0;
-	while (i != n)
+	while (i < (n - 1))
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = src[i];
 	return (dest);
 }
 
 /*int	main(void)
 {
 	char	src[] = "This is a test";
-	char	dest[] = "";
+	char	dest[] = "        ";
+	char	src2[] = "This is a test";
+	char	dest2[] = "        ";
 
-	printf("src:	%s\n", src);
-	printf("ft_strcpy: %s\n", ft_strncpy(dest, src, 14));
-	printf("dest:	%s\n", dest);
+	printf("src:		%s\n", src);
+	printf("dest:		%s\n", dest);
+	printf("ft_strncpy:	%s\n", ft_strncpy(dest, src, 8));
+	printf("src:		%s\n", src);
+	printf("dest:		%s\n", dest);
+	printf("\n");
+	printf("src2:		%s\n", src2);
+	printf("dest2:		%s\n", dest2);
+	printf("strncpy:	%s\n", strncpy(dest2, src2, 8));
+	printf("src2:		%s\n", src2);
+	printf("dest2:		%s\n", dest2);
 }*/
