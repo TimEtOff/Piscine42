7b869bbc4e6fdc82bfd93185fa22ed69af80a2b5
tgodefro <tgodefro@student.42lyon.fr>
2025-07-16 11:14:26 +0200

    c02/Finished ex12 (ft_print_memory)

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -6,23 +6,37 @@
 /*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 18:19:40 by tgodefro          #+#    #+#             */
-/*   Updated: 2025/07/15 18:48:22 by tgodefro         ###   ########lyon.fr   */
+/*   Updated: 2025/07/16 11:13:52 by tgodefro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
 /*
-	Imported from `ex11/ft_putstr_non_printable`.
+	Edited from `ex11/ft_putstr_non_printable`.
 */
-void	ft_put_hex_from_int(int nb, int size)
+void	ft_put_hex_from_int(long nb, int size)
 {
 	char	*hex_nbrs;
+	char	print;
 
 	hex_nbrs = "0123456789abcdef";
-	if (nb >= 16 || size > 1)
+	if ((nb >= 16 && size > 1) || size > 1)
 		ft_put_hex_from_int(nb / 16, size - 1);
-	write(1, &hex_nbrs[nb % 16], 1);
+	if (nb < 0)
+		print = '0';
+	else
+		print = hex_nbrs[nb % 16];
+	write(1, &print, 1);
 }
 
 /*
@@ -38,44 +52,66 @@ int	ft_char_is_printable(char chr)
 	return (res);
 }
 
-void	ft_putstr(char *str)
+void	ft_print_line(char *str, int line, int size)
 {
-	while (*str != '\0')
+	char	print_chars[17];
+	int		index;
+
+	index = 0;
+	while (index < 16)
 	{
-		write(1, str, 1);
-		str++;
+		if (index < size)
+		{
+			print_chars[index] = str[(16 * line + index)];
+			ft_put_hex_from_int((long) print_chars[index], 2);
+			if (!ft_char_is_printable(print_chars[index]))
+				print_chars[index] = '.';
+		}
+		else
+		{
+			ft_putstr("  ");
+			print_chars[index] = '\0';
+		}
+		if (index % 2 == 1)
+			ft_putstr(" ");
+		index++;
 	}
+	ft_putstr(print_chars);
+	ft_putstr("\n");
 }
 
 void	*ft_print_memory(void *addr, unsigned int size)
 {
 	unsigned int	line;
-	unsigned int	index;
 	char			*str;
+	int				left;
 
 	str = (char *) addr;
 	line = 0;
-	index = 0;
+	left = size;
+	if (size == 0)
+		return (addr);
 	while (line <= (size / 16))
 	{
-		ft_put_hex_from_int((int) str[(16 * line)], 16);
+		ft_put_hex_from_int((long) &str[(16 * line)], 16);
 		ft_putstr(": ");
-		while (index < 16)
-		{
-			ft_put_hex_from_int((int) str[(16 * line + index)], 2);
-			index++;
-		}
-		ft_putstr("\n");
+		ft_print_line(str, line, left);
+		left -= 16;
 		line++;
-		index = 0;
 	}
 	return (addr);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	char	*str;
+	int		test[] = {1, 2, 8, 6, 7, 3, 4, 2, 8, 0, 3};
 
 	str = "This is a test string for displaying the memory area";
-	ft_print_memory(str, 52);
-}
+	ft_print_memory(str, 56);
+	ft_putstr("\n");
+	str = "abcdefghijklmnopqrstuvwxyz0123456789";
+	ft_print_memory(str, 37);
+	ft_putstr("\n");
+	ft_print_memory(test, 92);
+}*/
