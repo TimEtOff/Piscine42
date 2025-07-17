3345ae17ad379fc4280a426494e36920cd10d862
tgodefro <tgodefro@student.42lyon.fr>
2025-07-17 15:22:30 +0200

    c03/Should have fixed ex05 (strlcat)

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,12 +6,11 @@
 /*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 15:25:58 by tgodefro          #+#    #+#             */
-/*   Updated: 2025/07/15 16:39:33 by tgodefro         ###   ########lyon.fr   */
+/*   Updated: 2025/07/17 15:21:12 by tgodefro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include <stdio.h>
-#include <bsd/string.h>*/
+
 
 /*
 	Imported from `c01/ex06`
@@ -31,46 +30,51 @@ int	ft_strlen(char *str)
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	unsigned int	dest_index;
-	unsigned int	src_index;
-	int				res;
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
 
-	dest_index = 0;
-	res = ft_strlen(dest) + ft_strlen(src);
-	while (dest[dest_index] != '\0')
-		dest_index++;
-	src_index = 0;
-	while ((dest_index + src_index) < size - 1)
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (size == 0)
+		return (src_len);
+	i = 0;
+	if (dest_len < (size - 1))
 	{
-		if (src[src_index] == '\0')
-			break ;
-		dest[dest_index + src_index] = src[src_index];
-		src_index++;
+		while (i < (size - dest_len - 1) && src[i] != '\0')
+		{
+			dest[dest_len + i] = src[i];
+			i++;
+		}
+		dest[dest_len + i] = '\0';
+		return (dest_len + src_len);
 	}
-	dest[dest_index + src_index] = '\0';
-	return (res);
+	else
+		return (dest_len + size + 1);
+
 }
 
-/*
-	Needs to compile with `-lbsd` flag
-*/
-/*int	main(void)
+#include <stdio.h>
+#include <bsd/string.h>
+
+// Needs to compile with `-lbsd` flag
+int	main(void)
 {
 	char	src[] = "This is a test";
-	char	dest[] = "Already here ";
+	char	dest[100] = "Already here ";
 	char	src2[] = "This is a test";
-	char	dest2[] = "Already here ";
+	char	dest2[100] = "Already here ";
 
 	printf("src:		%s\n", src);
 	printf("dest:		%s\n", dest);
-	printf("ft_strlcat:	%i\n", ft_strlcat(dest, src, 20));
+	printf("ft_strlcat:	%i\n", ft_strlcat(dest, src, 26));
 	printf("src:		%s\n", src);
 	printf("dest:		%s\n", dest);
 	printf("\n");
 	printf("src2:		%s\n", src2);
 	printf("dest2:		%s\n", dest2);
-	printf("strlcat:	%zu\n", strlcat(dest2, src2, 20));
+	printf("strlcat:	%zu\n", strlcat(dest2, src2, 26));
 	printf("src2:		%s\n", src2);
 	printf("dest2:		%s\n", dest2);
 	return (0);
-}*/
+}
