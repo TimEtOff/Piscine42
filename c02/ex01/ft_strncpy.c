cddbcea552b277d7129825233145883cba89c955
tgodefro <tgodefro@student.42lyon.fr>
2025-07-18 14:01:49 +0200

    c02/Ex01 (strncpy) should work

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 15:07:55 by tgodefro          #+#    #+#             */
-/*   Updated: 2025/07/16 17:38:17 by tgodefro         ###   ########lyon.fr   */
+/*   Updated: 2025/07/18 14:01:24 by tgodefro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,44 +29,42 @@ unsigned int	ft_strlen(char *str)
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
-	int				ended;
 
-	if (ft_strlen(dest) < n)
-		return (dest);
 	i = 0;
-	ended = 0;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
 	while (i < n)
 	{
-		if (src[i] == '\0')
-			ended = 1;
-		if (!ended)
-			dest[i] = src[i];
-		else
-			dest[i] = '\0';
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
 }
-/*
-#include <stdio.h>
+
+/*#include <stdio.h>
 #include <string.h>
 
 int	main(void)
 {
-	char	src[] = "This is a test";
-	char	dest[] = "                   ";
-	char	src2[] = "This is a test";
-	char	dest2[] = "                   ";
+	char	src[] = "Hello world";
+	char	dest[100];
+	char	src2[] = "Hello world";
+	char	dest2[100];
 
 	printf("src:		%s\n", src);
 	printf("dest:		%s\n", dest);
-	printf("ft_strncpy:	%s\n", ft_strncpy(dest, src, 8));
+	printf("ft_strncpy:	%s\n", ft_strncpy(dest, src, 5));
+	//printf("c:			%i\n", dest[12]);
 	printf("src:		%s\n", src);
 	printf("dest:		%s\n", dest);
 	printf("\n");
 	printf("src2:		%s\n", src2);
 	printf("dest2:		%s\n", dest2);
-	printf("strncpy:	%s\n", strncpy(dest2, src2, 8));
+	printf("strncpy:	%s\n", strncpy(dest2, src2, 5));
+	//printf("c:			%i\n", dest2[12]);
 	printf("src2:		%s\n", src2);
 	printf("dest2:		%s\n", dest2);
 }*/
