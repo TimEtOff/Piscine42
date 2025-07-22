6aa414eb999dd99ddef0c94437fb036e05278c2c
tgodefro <tgodefro@student.42lyon.fr>
2025-07-22 17:32:24 +0200

    c06/Ex00 (print_program_name)

---
+++ c06/ex00/ft_print_program_name.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 17:28:59 by tgodefro          #+#    #+#             */
+/*   Updated: 2025/07/22 17:31:32 by tgodefro         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 0)
+	{
+		ft_putstr(argv[0]);
+		ft_putstr("\n");
+	}
+}
