e404b1d72f3cc393a9b3239a4e6f62c68870d39a
tgodefro <tgodefro@student.42lyon.fr>
2025-07-22 17:40:52 +0200

    c06/Ex02 (rev_params)

---
+++ c06/ex02/ft_rev_params.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 17:39:37 by tgodefro          #+#    #+#             */
+/*   Updated: 2025/07/22 17:40:11 by tgodefro         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = argc - 1;
+	while (i > 0)
+	{
+		ft_putstr(argv[i]);
+		ft_putstr("\n");
+		i--;
+	}
+	return (0);
+}
