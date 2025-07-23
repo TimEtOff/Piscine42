55199b01f3cc47db798aeee4e2abee84bbeb66da
tgodefro <tgodefro@student.42lyon.fr>
2025-07-23 13:15:16 +0200

    c05/Made ex08 work (The Ten Queens)

--- c05/ex08/ft_ten_queens_puzzle.c
+++ c05/ex08/ft_ten_queens_puzzle.c
@@ -6,28 +6,72 @@
 /*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 14:54:37 by tgodefro          #+#    #+#             */
-/*   Updated: 2025/07/22 15:52:27 by tgodefro         ###   ########lyon.fr   */
+/*   Updated: 2025/07/23 13:14:43 by tgodefro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	is_valid(char *solution)
+int	is_valid(int *solution, int col, int row)
 {
 	int	res;
-	int	ind;
+	int	i;
 	int	diff;
 
 	res = 1;
-	ind = 0;
-	while (ind < 9)
+	i = col - 1;
+	diff = 1;
+	while (i >= 0 && res == 1)
 	{
-		diff = (solution[ind] - '0') - (solution[ind + 1] - '0');
-		if (diff < 0)
-			diff = -diff;
-		if (diff < 2)
-			res = 0;
-		ind++;
+		if (solution[i] == row)
+			res = 2;
+		if (row - diff >= 0 && solution[i] == row - diff)
+			res = 3;
+		else if (row + diff < 10 && solution[i] == row + diff)
+			res = 4;
+		i--;
+		diff++;
+	}
+	return (res);
+}
+
+void	print_solution(int *solution)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	while (i < 10)
+	{
+		c = solution[i] + '0';
+		write(1, &c, 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	find_solution(int *solution, int col, int *count)
+{
+	int	res;
+	int	i;
+
+	res = 0;
+	i = 0;
+	while (i < 10)
+	{
+		if (is_valid(solution, col, i) == 1)
+		{
+			res = 1;
+			solution[col] = i;
+			if (col < 9)
+				res = find_solution(solution, col + 1, count);
+			else
+			{
+				print_solution(solution);
+				(*count)++;
+			}
+		}
+		i++;
 	}
 	return (res);
 }
@@ -35,8 +79,10 @@ int	is_valid(char *solution)
 int	ft_ten_queens_puzzle(void)
 {
 	int		count;
+	int		solution[10];
 
 	count = 0;
+	find_solution(solution, 0, &count);
 	return (count);
 }
 
@@ -44,6 +90,6 @@ int	ft_ten_queens_puzzle(void)
 
 int	main(void)
 {
-	printf("%i\n", is_valid("0257948163"));
+	printf("%i\n", ft_ten_queens_puzzle());
 	return (0);
 }*/
