298c26bb007b764258fd09311e60eaf94a9f8de8
tgodefro <tgodefro@student.42lyon.fr>
2025-07-25 10:05:15 +0200

    c05/Fixed factorial after moulinette

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 17:47:18 by tgodefro          #+#    #+#             */
-/*   Updated: 2025/07/21 17:55:19 by tgodefro         ###   ########lyon.fr   */
+/*   Updated: 2025/07/25 10:02:46 by tgodefro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,25 @@ int	ft_iterative_factorial(int nb)
 {
 	int	res;
 
-	res = nb;
-	nb--;
-	while (nb >= 1)
+	if (nb < 0)
+		res = 0;
+	else if (nb == 0)
+		res = 1;
+	else
 	{
-		res *= nb;
+		res = nb;
 		nb--;
+		while (nb >= 1)
+		{
+			res *= nb;
+			nb--;
+		}
 	}
 	return (res);
 }
 
-/*#include <stdio.h>
-
-int	main(void)
+/*int	main(void)
 {
-	printf("%i\n", ft_iterative_factorial(10));
+	__builtin_printf("%i\n", ft_iterative_factorial(10));
 	return (0);
 }*/
--- c05/ex01/ft_recursive_factorial.c
+++ c05/ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 17:58:28 by tgodefro          #+#    #+#             */
-/*   Updated: 2025/07/21 18:01:07 by tgodefro         ###   ########lyon.fr   */
+/*   Updated: 2025/07/25 10:03:58 by tgodefro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@ int	ft_recursive_factorial(int nb)
 	int	res;
 
 	res = nb;
-	if (nb > 1)
+	if (nb < 0)
+		res = 0;
+	else if (nb == 0)
+		res = 1;
+	else if (nb > 1)
 		res *= ft_recursive_factorial(nb - 1);
 	return (res);
 }
 
-/*#include <stdio.h>
-
-int	main(void)
+/*int	main(void)
 {
-	printf("%i\n", ft_recursive_factorial(10));
+	__builtin_printf("%i\n", ft_recursive_factorial(10));
 	return (0);
 }*/
