b0cfe40f4a8c9dcaaaeef7a0643871962583dc7a
tgodefro <tgodefro@student.42lyon.fr>
2025-07-25 14:15:31 +0200

    c07/Ex04 (convert_base)

---
+++ c07/ex04/ft_convert_base.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 11:59:04 by tgodefro          #+#    #+#             */
+/*   Updated: 2025/07/25 14:15:06 by tgodefro         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+long	ft_atoi_base(char *str, char *base);
+int		check_base(char *base);
+int		ft_strlen(char *str);
+
+void	str_append_char(char *str, char append)
+{
+	int	i;
+	int	str_len;
+
+	str_len = ft_strlen(str);
+	i = 0;
+	str[str_len] = append;
+	str[str_len + 1] = '\0';
+}
+
+void	recursive_write(long nbr, char *base, int base_size, char *res)
+{
+	if (nbr > 0)
+	{
+		if (nbr > 0)
+			recursive_write(nbr / base_size, base, base_size, res);
+		str_append_char(res, base[nbr % base_size]);
+	}
+}
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	long	atoied_nbr;
+	int		base_size;
+	char	*res;
+
+
+	if (!check_base(base_to))
+		return (NULL);
+	atoied_nbr = ft_atoi_base(nbr, base_from);
+	base_size = ft_strlen(base_to);
+	res = "";
+	if (check_base(base_to)
+		&& -2147483648 <= atoied_nbr && atoied_nbr <= 2147483647)
+	{
+		res = malloc(sizeof(char) * 100);
+		res[0] = '\0';
+		if (atoied_nbr < 0)
+		{
+			str_append_char(res, '-');
+			atoied_nbr = -atoied_nbr;
+		}
+		recursive_write(atoied_nbr, base_to, base_size, res);
+	}
+	else
+		return (NULL);
+	return (res);
+}
+
+/*int	main(void)
+{
+	char	*res;
+
+	res = ft_convert_base(" -+--80000000hy", "0123456789ABCDEF", "01");
+	__builtin_printf("%s\n", res);
+	res = ft_convert_base("lX", "XavNiel", "0123456789");
+	__builtin_printf("%s\n", res);
+	free(res);
+	return (0);
+}*/
---
+++ c07/ex04/ft_convert_base2.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 17:09:35 by tgodefro          #+#    #+#             */
+/*   Updated: 2025/07/25 14:08:58 by tgodefro         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_negative(char *str, int *index)
+{
+	int	negatives;
+
+	negatives = 0;
+	while (str[*index] == '+' || str[*index] == '-')
+	{
+		if (str[*index] == '-')
+			negatives++;
+		(*index)++;
+	}
+	return (negatives % 2);
+}
+
+int	check_base(char *base)
+{
+	int	res;
+	int	i;
+	int	i_check;
+
+	res = 1;
+	i = 0;
+	i_check = 1;
+	if (base[i] == '\0' || base[i + 1] == '\0')
+		res = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == '+' || base[i] == '-' || base[i] == ' '
+			|| base[i] == '\f' || base[i] == '\n' || base[i] == '\r'
+			|| base[i] == '\t' || base[i] == '\v')
+			res = 0;
+		while (base[i_check] != '\0')
+		{
+			if (i != i_check && base[i] == base[i_check])
+				res = 0;
+			i_check++;
+		}
+		i_check = 0;
+		i++;
+	}
+	return (res);
+}
+
+int	index_in_base(char chr, char *base)
+{
+	int	ind;
+
+	ind = 0;
+	while (base[ind] != '\0')
+	{
+		if (base[ind] == chr)
+			return (ind);
+		ind++;
+	}
+	return (-1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+long	ft_atoi_base(char *str, char *base)
+{
+	int		index;
+	int		negative;
+	long	res;
+
+	res = 0;
+	if (check_base(base))
+	{
+		index = 0;
+		while (str[index] == ' ' || str[index] == '\f' || str[index] == '\n'
+			|| str[index] == '\r' || str[index] == '\t' || str[index] == '\v')
+			index++;
+		negative = ft_is_negative(str, &index);
+
+		while (index_in_base(str[index], base) >= 0)
+		{
+			res *= ft_strlen(base);
+			res += index_in_base(str[index], base);
+			index++;
+		}
+		if (negative)
+			res = -res;
+	}
+	return (res);
+}
