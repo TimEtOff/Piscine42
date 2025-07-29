784bcd07f91f64e11981bc865fe50ff76ab7edc9
TimEtOff <97166376+TimEtOff@users.noreply.github.com>
2025-07-29 21:10:20 +0000

    c08/Ex03

---
+++ c08/ex03/test.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgodefro <tgodefro@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 20:42:51 by tgodefro          #+#    #+#             */
+/*   Updated: 2025/07/29 21:08:04 by tgodefro         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_point.h"
+
+void	set_point(t_point *point)
+{
+	point->x = 42;
+	point->y = 21;
+}
+
+int	main(void)
+{
+	t_point	point;
+
+	set_point(&point);
+	return (0);
+}
